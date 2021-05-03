da37f75a10b6d04d6ecf44eb139fc8d8ac333b11
kutarobo <kutarobo@naver.com>
2021-05-04 03:44:26 +0900

    feat: Add InputUtils and racingCarGame run
    자동차경주게임을 실행하는 기능 및 입력 기능 추가
    -차량이름입력 기능 추가
    -시도회수입력 기능 추가
    -경주게임을 실행하는 기능 추가.
    -차량객체 생성중 예외발생시 재귀로 다시 게임실행하도록 기능 추가

--- src/main/java/racingcar/RacingCarApp.java
+++ src/main/java/racingcar/RacingCarApp.java
@@ -1,2 +1,9 @@
-package racingcar;public class RacingCarApp {
+package racingcar;
+
+public class RacingCarApp {
+	public static void main(String[] args) {
+		RacingCarGame racingCarGame = new RacingCarGame();
+		racingCarGame.run();
+	}
 }
+
--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,2 +1,42 @@
-package racingcar;public class RacingCarGame {
+package racingcar;
+
+import java.util.Scanner;
+
+public class RacingCarGame {
+	private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+	private int tryCount;
+	private InputUtils inputUtils;
+	private ParticipantCars participantCars;
+
+	public RacingCarGame() {
+		this.tryCount = 0;
+		this.inputUtils = new InputUtils(new Scanner(System.in));
+	}
+
+	public void run() {
+		try {
+			participantCars = new ParticipantCars(getInputData(INPUT_CAR_NAME_MESSAGE));
+		} catch (IllegalArgumentException iae) {
+			System.out.println(iae.getMessage());
+			run();
+			return;
+		}
+		inputTryCount();
+	}
+
+	private String getInputData(String outputMessage) {
+		System.out.println(outputMessage);
+		return inputUtils.getInputData();
+	}
+
+	private void inputTryCount() {
+		String tryCount = getInputData(INPUT_TRY_COUNT_MESSAGE);
+		if (!tryCount.chars().allMatch(Character::isDigit) && Integer.parseInt(tryCount) > 0) {
+			inputTryCount();
+			return;
+		}
+		this.tryCount = Integer.parseInt(tryCount);
+	}
 }
