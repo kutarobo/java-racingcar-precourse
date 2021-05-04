f140896f6459cf947a6f5597240b05529fa47df2
kutarobo <kutarobo@naver.com>
2021-05-04 15:32:40 +0900

    refactor: Update winner message
    승자메세지 접미사부분 상수로 등록

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -6,6 +6,7 @@ public class RacingCarGame {
 	private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 	private static final String RESULT_MESSAGE = "실행 결과";
+	private static final String WINNER_SUFFIX_MESSAGE = "가 최종 우승했습니다.";
 
 	private int tryCount;
 	private InputUtils inputUtils;
@@ -51,6 +52,6 @@ public class RacingCarGame {
 	}
 
 	private void endGame() {
-		System.out.println(participantCars.getWinnerCarNames() + "가 최종 우승 했습니다.");
+		System.out.println(participantCars.getWinnerCarNames() + WINNER_SUFFIX_MESSAGE);
 	}
 }
